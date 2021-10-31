e88c48c25e6c2f531673d17b124cf87d495553f5
Himanshu Dhaka <himanshudhaka1701@gmail.com>
2021-10-31 18:02:35 +0530

    Angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+
+import { gsap } from 'gsap'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drewziggymoon';
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.layerAnimation();
+  }
+
+  openDialog() {
+    this.dialog.open(loginDialog);
+  }
+
+  layerAnimation(){
+    let tl = gsap.timeline()
+    gsap.defaults({
+      // ease: "power2.in", 
+      duration: 0.5
+    });
+    tl.from('.header', {y: -64}, 0)
+      .from('.navvlink', {y: -64, stagger: 0.1}, 0.5)
+  }
+
 }
+
+@Component({
+  selector: 'login-dialog',
+  templateUrl: 'login-dialog.html',
+})
+export class loginDialog {}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,46 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, loginDialog } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EventsComponent } from './pages/events/events.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatListModule} from '@angular/material/list';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EventsComponent,
+    loginDialog
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
